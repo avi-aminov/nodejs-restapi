31dcf2e9deb76746814a4c2c6b9f6ea62ca26979
avi-aminov <aviaminov.co.il@gmail.com>
2019-08-11 23:02:28 +0300

    mysql get all clients + delete by id

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 ﻿const mysql = require('mysql');
+const express = require('express');
+const bodyparser = require('body-parser');
+
+let app = express();
+app.use(bodyparser.json());
 
 let mysqlConnection = mysql.createConnection({
 	host: 'localhost',
@@ -14,4 +19,66 @@ mysqlConnection.connect((err)=>{
 	}else{
 		console.log("mysqlConnection error \n" + JSON.stringify(err,undefined,2));
 	}
+});
+
+app.listen(3000, ()=> {
+	console.log("Exrpess server is runing at port no: 3000");
+});
+
+
+// Get all clients
+app.get('/clients', (req, res)=>{
+	mysqlConnection.query('SELECT * FROM clients', (err, rows, fields)=>{
+		if(!err){
+			res.send(rows);
+		}else{
+			console.log(err);
+		}
+	});
+});
+
+
+// Get clients by ID
+app.get('/clients/:id', (req, res)=>{
+	mysqlConnection.query('SELECT * FROM `clients` WHERE clientId = ?', [req.params.id],
+	(err, rows, fields)=>{
+		if(!err){
+			res.send(rows);
+		}else{
+			console.log(err);
+		}
+	});
+});
+
+
+// Delete clients by ID
+app.delete('/clients/:id', (req, res)=>{
+	mysqlConnection.query('DELETE FROM clients WHERE clientId = ?', [req.params.id],
+	(err, rows, fields)=>{
+		if(!err){
+			res.send('Deleted seccessfully');
+		}else{
+			console.log(err);
+		}
+	});
+});
+
+
+// Insert clients
+app.post('/add-client/', (req, res)=>{
+	
+	let emp = req.body;
+
+	console.log(emp);
+	
+	/*
+	mysqlConnection.query("INSERT INTO `clients` (`clientId`, `firstName`, `lastName`, `age`, `adress`, `dateEntered`, `dateUpdated`) VALUES (null, 'cdf', 'rgre', '', 'tel aviv', '2019-08-11 00:00:00', '2019-08-11 18:50:19')", [req.params.id],
+	(err, rows, fields)=>{
+		if(!err){
+			res.send('Insert seccessfully');
+		}else{
+			console.log(err);
+		}
+	});
+	*/
 });
