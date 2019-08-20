50fe100e20f611c6f5ff70f4f4a9c99611fd477b
avia <avia@aspireglobal.com>
2019-08-20 18:24:28 +0300

    add router and posts model

--- index.js
+++ index.js
@@ -1,95 +1,25 @@
 ﻿	const express = require('express');
-	const bodyparser = require('body-parser');
+	const bodyParser = require('body-parser');
 	const dbConnection = require('./db');
-	
-	//const mysql = require('mysql');
-	const mongoClient = require('mongodb').MongoClient;
-	const objectID = require('mongodb').ObjectID;
-
-	// EXAMPLE FOR MYSQL DB
-	//const clients = require('./model/clients');
 	
 	const app = express();
 	
-	dbConnection.connect((err)=>{
-		if(err){
-			return console.log(err);
-		}
-		app.listen(3000, ()=> {
-			console.log("Exrpess server is runing at port No.: 3000");
+	app.use(bodyParser.json());
+	app.use('/api', require('./model/posts'));
+	
+	
+	app.listen(process.env.port || 3000, ()=> {
+		console.log("Express server is running");
+		dbConnection.connect((err)=>{
+			if(err){
+				return console.log(err);
+			}else{
+				console.log("mongoDB connected");
+			}
 		});
-	})	
-	
-	app.use(bodyparser.urlencoded({ extended : true}));
-	app.use(bodyparser.json());
-	
-	app.get('/api/artists', (req, res)=>{
-		dbConnection.get().collection("artists").find().toArray((err, result)=>{
-			if(err){
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.send(result);
-		});	
-	})
+	});
 	
 	
-	app.post('/api/artists', (req, res)=>{
-		dbConnection.get().collection('artists').insertOne(
-		{name : req.body.name}, 
-		(err, result)=>{
-			if(err){
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.send(result);
-		});
-	})
-
-	
-	app.get('/api/artist/:id', (req, res)=>{
-		dbConnection.get().collection("artists").findOne(
-		{ _id: objectID(req.params.id)}, 
-		(err, doc)=>{
-			if(err){
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.send(doc);
-		})
-	})
-	
-	
-	app.put('/api/artist/:id', (req, res)=>{
-		dbConnection.get().collection("artists").updateOne(
-			{	_id: objectID(req.params.id)},
-			{ 	$set: {name: req.body.name}}, 
-			(err, result)=>{
-				if(err){
-					console.log(err);
-					return res.sendStatus(500);
-				}
-				res.sendStatus(200);
-			}
-		)
-	})
-	
-	
-	app.delete('/api/artist/:id', (req, res)=>{
-		dbConnection.get().collection("artists").deleteOne(
-		{ _id: objectID(req.params.id)}, 
-		(err, doc)=>{
-			if(err){
-				console.log(err);
-				return res.sendStatus(500);
-			}
-			res.sendStatus(200);
-		})
-	})
-	
-	
-	
-
 	
 	
 	// EXAMPLE FOR MYSQL DB
---
+++ model/posts.js
@@ -0,0 +1,90 @@
+﻿	const express = require('express');
+	const router = express.Router();
+	const mongoClient = require('mongodb').MongoClient;
+	const objectID = require('mongodb').ObjectID;
+	const dbConn = require('../config');
+	const dbConnection = require('../db');
+	
+	// get all posts rows
+	router.get('/posts', (req, res)=>{
+		dbConnection.get().collection("posts").find().toArray((err, result)=>{
+			try{
+				if(!err){
+					res.send(result);
+				}else{
+					throw err;
+				}
+			}catch(error){
+				res.status(500).send(error);
+			}
+		});
+		
+	});
+	
+	// insert one post
+	router.post('/post', (req, res)=>{
+		dbConnection.get().collection('posts').insertOne(
+		{
+			title : req.body.title,
+			slug : req.body.slug,
+			body : req.body.body
+		}, 
+		(err, result)=>{
+			if(err){
+				console.log(err);
+				return res.sendStatus(500);
+			}
+			res.send(result);
+		});
+	});
+	
+	// select post by id
+	router.get('/post/:id', (req, res)=>{
+		dbConnection.get().collection("posts").findOne(
+		{ 
+			_id : objectID(req.params.id)
+		}, 
+		(err, doc)=>{
+			if(err){
+				console.log(err);
+				return res.sendStatus(500);
+			}
+			res.send(doc);
+		});
+	});
+	
+	// update post by id
+	router.put('/post/:id', (req, res)=>{
+		dbConnection.get().collection("posts").updateOne(
+			{	_id: objectID(req.params.id)},
+			{ 	$set: {
+						title : req.body.title,
+						slug : req.body.slug,
+						body : req.body.body
+					}
+			}, 
+			(err, result)=>{
+				if(err){
+					console.log(err);
+					return res.sendStatus(500);
+				}
+				res.sendStatus(200);
+			}
+		);
+	});
+	
+	// delete post by id
+	router.delete('/post/:id', (req, res)=>{
+		dbConnection.get().collection("posts").deleteOne(
+		{ _id: objectID(req.params.id)}, 
+		(err, doc)=>{
+			if(err){
+				console.log(err);
+				return res.sendStatus(500);
+			}
+			res.sendStatus(200);
+		});
+	});
+	
+
+	module.exports = router;
